7c2ac1a3796be15daee444f07a1f903bbc1d8e5a
aberu78 <aberu78@gmail.com>
2020-09-24 00:35:26 -0500

    creating Student Class __init__ funcction defined.

---
+++ student.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, first, last, student_id, grade):
+        self.first_name = first
+        self.last_name = last
+        self.person_id = student_id
+        self.student_grade = grade
+
+
+person = Student("Kaori", "gaddis", 111, 45)
