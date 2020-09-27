b42c9808c0a68c6348a0d625f2aa5503a3182a5a
aberu78 <aberu78@gmail.com>
2020-09-26 20:51:03 -0500

    print all student is modified print grades is modified print average is entered.

--- student.py
+++ student.py
@@ -4,54 +4,73 @@ import os
 
 
 class Student:
-    def __init__(self, num_id, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.id = num_id
-        self.grades = {}
 
-    def add_grade(self, sub, score):
-        if sub in self.grades:
-            self.grades[sub].append(score)
+    def __init__(self, num_id, first_name, last_name):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__id = num_id
+        self.__grades = {}
+
+    def add_grade(self, subject, grade):
+        if subject in self.__grades:
+            self.__grades[subject].append(grade)
         else:
-            self.grades[sub] = [score]
-        print("grade is added", self.grades)
+            self.__grades[subject] = [grade]
+        print("a grade is added")
+
+    def grade_exist(self):
+        if len(self.__grades.keys()) > 0:
+            return True
+        else:
+            return False
 
     def print_student(self):
-        print("print_student , ID: %s , Name: %s %s " % (self.id, self.first_name, self.last_name))
+        print("ID: %s ; Name: %s %s " % (self.__id, self.__first_name, self.__last_name))
 
-    def print_grade(self):
-        for sub in self.grades:
-            print("print_grade ", sub, self.grades[sub])
+    def print_grades(self):
+        print("Entering print_grades()")
+        if self.grade_exist():
+            for sub in self.__grades:
+                    print("ID: %i Subject: %s grades: %s " % (self.__id, sub, self.__grades[sub]))
+        else:
+            print("No grades entered for this student")
+
+    def print_avg(self):
+        print("Entering print_avg() ")
+        if self.grade_exist():
+            avg = 0.0
+            for subject in self.__grades:
+                    avg = sum(self.__grades[subject]) / len(self.__grades[subject])
+                    print("%s average is %i" % (subject, avg))
+        else:
+            print("No grades entered for this student ID")
+
 
     def __str__(self):
-        return self.last_name + ", " + self.first_name
+        return "ID: %i  %s %s " % (self.__id, self.__last_name, self.__first_name)
 
     def __repr__(self):
         return self.__str__()
 
 
-# check to see if the file exists, then load  the file, else return the empty dictionary
+def add_student(student_num, fname, lname):
+    tmp_student = Student(student_num, fname, lname)
+    return tmp_student
+
+
 def load():
-    """ load student information from file"""
     info = {}
-    if os.path.exists("student_test.txt"):
-        with open("student_test.txt", "rb") as input_char:
+    if os.path.exists("student_test3.txt"):
+        with open("student_test3.txt", "rb") as input_char:
             info = pickle.load(input_char)
             print("initial load ", info)
     return info
 
 
 def save():
-    """ save all student info to file"""
-    with open("student.txt", "wb") as output:
+    with open("student_test3.txt", "wb") as output:
         pickle.dump(student_info, output)
         print("pickle success")
-
-
-def add_student(student_num,fname, lname):
-    tmp_student = Student(student_num, fname, lname)
-    return tmp_student
 
 
 student_info = load()  # load student information from a file
@@ -75,8 +94,8 @@ while user_cond:  # program will run until q is selected
             check_id = True  # return false when id is entered correctly
 
             while check_name:
-                first = input("Please enter first name :")
-                last = input("Please enter last name :")
+                first = input("First name :")
+                last = input("Last name :")
 
                 if not first.isnumeric() and not last.isnumeric():
                     first = re.sub(' +', ' ', first.strip())
@@ -85,7 +104,7 @@ while user_cond:  # program will run until q is selected
                     student_last = last
                     check_name = False
                 else:
-                    print("invalid names, please try again \n")
+                    print("invalid names,try again \n")
 
             while check_id:
                 tmp = input("Please enter student ID :")
@@ -98,27 +117,12 @@ while user_cond:  # program will run until q is selected
                         check_id = False
                         isValid = False
                     else:
-                        print("Student ID you entered already exists, please choose another ID number\n")
+                        print("Student ID already exists, please choose another ID number\n")
                 else:
-                    print("Invalid id, please enter ID\n")
+                    print("Invalid id, try again \n")
 
             student_info[student_id] = add_student(student_id, student_first, student_last)  # save student information
 
-    elif user_input == "d":  # remove the student
-        isValid = True  # return false when information is deleted from dictionary
-        while isValid:
-            remove_id = input("Please enter student ID :")
-
-            if remove_id.isnumeric():
-                remove_id = int(remove_id)
-                if remove_id in student_info:
-                    del student_info[remove_id]  # check to see the keyword exists and valid number
-                    isValid = False
-                    print("student removed\n")
-                else:
-                    print("student id you entered does not exists, please enter valid number\n")
-            else:
-                print("invalid id number, please enter student id\n")
     elif user_input == "g":
         isValid = True  # return false when information is deleted from dictionary
 
@@ -126,22 +130,40 @@ while user_cond:  # program will run until q is selected
             add_id = input("Please enter student ID :")
             if add_id.isnumeric():
                 add_id = int(add_id)
+                print("inside loop")
                 if add_id in student_info:
-                    subject = input("Please enter a Class Name")
-                    grade = input("please enter a grade")
-                    student_info[add_id].add_grade(subject, grade)
+                    subject = input("Class Name ?")
+                    grade = input("Grade ?")
+                    student_info[add_id].add_grade(subject, int(grade))
                     isValid = False
-                    print("end of adding grade")
             else:
-                print("ID you entered is invalid, try again")
+                print("Invalid ID, try again")
 
     elif user_input == "p":
-        print("print function", student_info)  # print student information
+
+        print("Please enter 1 to print all student Names")
+        print("Please enter 2 to all grades and average\n")
+        option = input("<<<<<<< ")
+        option = option.strip()
+
+        if int(option) == 1:
+            for student_id in student_info:
+                student_info[student_id].print_student()  # print student information
+        elif int(option) == 2:
+            student_id = input("Student ID ?")
+            if int(student_id) in student_info:
+                student_info[int(student_id)].print_grades()
+                student_info[int(student_id)].print_avg()
+            else:
+                print("invalid student id, try again")
+        else:
+            print("Invalid option error")
+
     elif user_input == "q":
         user_cond = False
         save()
     else:
-        print("Please enter a valid option\n")
+        print("Invalid option, try again\n")
 
 print(student_info)
 print("Good bye")
